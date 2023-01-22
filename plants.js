394f9cfcec7e37a027b5802dba433d8be489b555
Mariana <mariana.balanchuk@gmail.com>
2023-01-23 00:02:49 +0100

    added next section, configured staff hover

--- plants.js
+++ plants.js
@@ -1,6 +1,6 @@
 const swiper = new Swiper(".swiper", {
   slidesPerView: 1,
-  spaceBetween: 10,
+  spaceBetween: 20,
   loop: true,
   keyboard: {
     enabled: true,
@@ -8,24 +8,21 @@ const swiper = new Swiper(".swiper", {
 
   pagination: {
     el: ".swiper-pagination",
-    dynamicBullets: true,
     clickable: true,
   },
 
   breakpoints: {
-    420: {
+    768: {
       slidesPerView: 2,
       spaceBetween: 20,
     },
-    768: {
+    992: {
       slidesPerView: 3,
       spaceBetween: 30,
     },
-    992: {
+    1440: {
       slidesPerView: 4,
       spaceBetween: 40,
     },
   },
 });
-
-alert("working");
