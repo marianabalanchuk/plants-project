227dcaffb61dfa35d6966e0eae0b4ff14ae2f404
Mariana <mariana.balanchuk@gmail.com>
2023-01-22 22:31:04 +0100

    finished styling staff section

--- plants.js
+++ plants.js
@@ -2,10 +2,29 @@ const swiper = new Swiper(".swiper", {
   slidesPerView: 1,
   spaceBetween: 10,
   loop: true,
+  keyboard: {
+    enabled: true,
+  },
 
   pagination: {
     el: ".swiper-pagination",
     dynamicBullets: true,
+    clickable: true,
+  },
+
+  breakpoints: {
+    420: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+    992: {
+      slidesPerView: 4,
+      spaceBetween: 40,
+    },
   },
 });
 
