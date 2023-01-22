4d5e0354906d1892f2bc56ec9738bb1bd3d56af1
Mariana <mariana.balanchuk@gmail.com>
2023-01-22 21:47:26 +0100

    fixed slider, changed photos to grey png squares

--- plants.js
+++ plants.js
@@ -1,13 +1,11 @@
 const swiper = new Swiper(".swiper", {
+  slidesPerView: 1,
+  spaceBetween: 10,
   loop: true,
 
   pagination: {
     el: ".swiper-pagination",
-  },
-
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
+    dynamicBullets: true,
   },
 });
 
